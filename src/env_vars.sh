04f3b0db15183d88087a677ff4eea8146b0cc900
raybergholm <raybergholm@gmail.com>
2024-05-05 17:24:33 +0300

    add jdk and ruby paths

--- src/env_vars.sh
+++ src/env_vars.sh
@@ -10,12 +10,21 @@ export NVM_DIR="$HOME/.nvm"
 [ -s "/usr/local/opt/nvm/nvm.sh" ] && . "/usr/local/opt/nvm/nvm.sh"  # This loads nvm
 [ -s "/usr/local/opt/nvm/etc/bash_completion.d/nvm" ] && . "/usr/local/opt/nvm/etc/bash_completion.d/nvm"  # This loads nvm bash_completion
 
+### JAVA
+
+java_openjdk_path="/usr/local/opt/openjdk/bin"
+PATH="${PATH}:${java_openjdk_path}"
+
+### RUBY
+ruby_path="/usr/local/opt/ruby/bin"
+PATH="${PATH}:${ruby_path}"
+
 ### PYTHON
 
 python3_path="$HOME/Library/Python/3.9/bin"
-poetry_path="$HOME/.poetry/bin"
+# poetry_path="$HOME/.poetry/bin"
 
 PATH="${PATH}:${python3_path}"
-PATH="${PATH}:${poetry_path}"
+# PATH="${PATH}:${poetry_path}"
 
 export PATH
