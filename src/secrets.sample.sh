7febeaf95f6f401b50083f3827ccdcc4c29bd4bc
raybergholm <raybergholm@gmail.com>
2025-06-06 11:50:13 +0300

    update gitignore

---
+++ src/secrets.sample.sh
@@ -0,0 +1,5 @@
+# Sample for credentials.sh
+# This can be set up the same as the env_vars.sh file, only with default .gitignore support.
+# Place your credentials or secrets here so that they don't get accidentally committed.
+
+SOME_PRIVATE_TOKEN=example_value
