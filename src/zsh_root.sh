a2b25c3ff78f1dad1b2597a60ec837a148fe8112
raybergholm <raybergholm@gmail.com>
2025-06-06 11:50:08 +0300

    move entry file

--- src/zsh_root.sh
+++
@@ -1,4 +0,0 @@
-source $TORTOISE_ROOT/shell_functions.sh 
-source $TORTOISE_ROOT/env_vars.sh 
-source $TORTOISE_ROOT/credentials.sh
-source $TORTOISE_ROOT/aliases.sh
---
+++ zsh_root.sh
@@ -0,0 +1,4 @@
+source $TORTOISE_ROOT/src/env_vars.sh 
+source $TORTOISE_ROOT/src/secrets.sh
+source $TORTOISE_ROOT/src/aliases.sh
+source $TORTOISE_ROOT/src/shell_functions.sh 
