efee008c499ae816a9411e2135eccfb819c3534a
raybergholm <raybergholm@gmail.com>
2025-06-06 16:53:21 +0300

    update secrets file comments

--- src/secrets.sample.sh
+++ src/secrets.sample.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-# Sample for credentials.sh
+# Sample for secrets.sh
 # This can be set up the same as the env_vars.sh file, only with default .gitignore support.
-# Place your credentials or secrets here so that they don't get accidentally committed.
+# Place your secrets here so that they don't get accidentally committed.
 
 SOME_PRIVATE_TOKEN=example_value
