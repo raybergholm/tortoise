1f047a84d1f0147395f63bcfd412e0ade8e63745
Raymond Bergholm <raymond.bergholm@sok.fi>
2026-04-30 10:58:15 +0300

    update secrets.sample

--- src/secrets.sample.sh
+++ src/secrets.sample.sh
@@ -1,5 +1,7 @@
-# Sample for credentials.sh
+#!/bin/sh
+
+# Sample for secrets.sh
 # This can be set up the same as the env_vars.sh file, only with default .gitignore support.
-# Place your credentials or secrets here so that they don't get accidentally committed.
+# Place your secrets here so that they don't get accidentally committed.
 
 SOME_PRIVATE_TOKEN=example_value
